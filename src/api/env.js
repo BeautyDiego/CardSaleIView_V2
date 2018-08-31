2ddb3bdc81dc64394e3a68d8e2aed27638976dfc
blind181 <blind181@126.com>
2018-08-31 09:52:39 +0800

    publish

--- src/api/env.js
+++ src/api/env.js
@@ -10,10 +10,10 @@ let baseUrl = 'http://localhost:5883'
 let frontWebUrl = 'http://localhost:8080'
 
 if (process.env.NODE_ENV === 'production') {
-  // baseUrl = 'http://www.chaodawulian.com:8002'
-  // frontWebUrl = 'http://www.chaodawulian.com'
-    baseUrl = 'http://ss.hbbdmc.com:8002'
-    frontWebUrl = 'http://ss.hbbdmc.com'
+   baseUrl = 'http://www.chaodawulian.com:8002'
+   frontWebUrl = 'http://www.chaodawulian.com'
+  //   baseUrl = 'http://ss.hbbdmc.com:8002'
+  //   frontWebUrl = 'http://ss.hbbdmc.com'
 }
 
 
